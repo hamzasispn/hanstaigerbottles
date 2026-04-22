483410901c76c25d45ce16421f81afebaba6db39
hamzasispn <mh6824213@gmail.com>
2026-04-22 05:44:28 +0500

    Create more new  2 Sections

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Alpine from 'alpinejs';
+import Swiper from 'swiper';
+import 'swiper/css';
 import './tailwind.css';
 import './main.scss';
 
 window.Alpine = Alpine;
+window.Swiper = Swiper;
+
 Alpine.start();
