5b1033f6b8e167ffeee497a5a66b67a92a3e255e
hamzasispn <talha.sispn@gmail.com>
2026-04-23 02:43:32 +0500

    updates

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Alpine from 'alpinejs';
 import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './tailwind.css';
 import './main.scss';
 
 window.Alpine = Alpine;
 window.Swiper = Swiper;
+window.Navigation = Navigation;
+window.Pagination = Pagination;
 
 Alpine.start();
